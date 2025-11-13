a0387350fe2cecd61d423136e34a44883c343997
cmdrumright <cdrumright@protonmail.com>
2025-11-13 11:22:30 -0600

    Implemented main algorithm in main module

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+import { generateMoviesHTML } from './movie-converter.js';
+import { renderMoviesToDOM } from './movie-render.js';
+
+const movieHTML = generateMoviesHTML();
+
+renderMoviesToDOM(movieHTML);
+
+
